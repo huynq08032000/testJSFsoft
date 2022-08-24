0c17c05d806ecf08fef341b84eff69a51eccc199
huynq08032000 <huynq08032000@gmail.com>
2022-08-24 13:02:17 +0700

    commit arrSearch

--- ArraySearch.js
+++ ArraySearch.js
@@ -1,22 +1,20 @@
 function numberOfItems(arr, item) {
-    // Write the code that goes here
-    let count = 0 ;
-    arr.forEach((el) => {
-      if (typeof(el) === 'object') {
-        el.forEach((el)=>{
-          if (el===item) count++;
-        })    
+  // Write the code that goes here
+  let count = 0 ;
+  arr.forEach((el) => {
+    if (typeof(el) === 'object') {
+      count = count + numberOfItems(el, item);
       } else {
-        if (el === item) count++
-      }
-    })
-    return count;
-  }
-  
-  var arr = [
-    25,
-    "apple",
-    ["banana", "strawberry", "apple", 25],
-  ];
-  console.log(numberOfItems(arr, 25));
-  console.log(numberOfItems(arr, "apple"));
+      if (el === item) count++
+    }
+  })
+  return count;
+}
+
+var arr = [
+  25,
+  "apple",
+  ["banana", "strawberry", "apple", 25]
+];
+console.log(numberOfItems(arr, 25));
+console.log(numberOfItems(arr, "apple"));
