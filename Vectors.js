b4be6847e34d172d09665cfb94245d8316977205
huynq08032000 <huynq08032000@gmail.com>
2022-08-24 15:42:33 +0700

    commit

--- Vectors.js
+++ Vectors.js
@@ -1,19 +1,18 @@
 function findShortest(vectors) {
-    // Write the code that goes here
-    const lengthVecs = []
-    vectors.forEach((el) => {
-      const rs = el.reduce((acc,cur)=> {
-        return acc+=cur*cur
-      },0)
-      lengthVecs.push(rs)
-    })
-    const minValue = Math.min(...lengthVecs)
-    const index = lengthVecs.findIndex((val) => val === minValue)
-    return vectors[index]
-  }
-  
-  var vectors = [[1, 1, 1], [2, 2, 2], [3, 3, 3]];
-  var shortest = findShortest(vectors);
-  console.log(shortest);
-  
-  
+  // Write the code that goes here
+  const lengthVecs = []
+  vectors.forEach((el) => {
+    const rs = el.reduce((acc,cur)=> {
+      return acc+=cur*cur
+    },0)
+    lengthVecs.push(rs)
+  })
+  const rsLengthVecs = lengthVecs.map(el => Math.sqrt(el))
+  const minValue = Math.min(...rsLengthVecs)
+  const index = rsLengthVecs.findIndex((val) => val === minValue)
+  return vectors[index]
+}
+
+var vectors = [[1, 1, 1], [2, 2, 2], [3, 3, 3]];
+var shortest = findShortest(vectors);
+console.log(shortest);
